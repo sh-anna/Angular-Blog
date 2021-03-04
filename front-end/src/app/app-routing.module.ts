05fc9ccbdde933d994330a73c840bb7abc13549c
Sharipkin Anna <sharipkinanna@gmail.com>
2021-03-04 13:17:42 +0200

    Add files via upload

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard'
+import { PostComponent } from './post/post.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'reg', component: RegComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'post/:id', component: PostComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
